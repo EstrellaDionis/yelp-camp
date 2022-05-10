0131d1f89e72e37039164fda0589f6149cf7c3f1
EstrellaDionis <EstrellaDionis@gmail.com>
2022-05-10 19:22:53 -0400

    created route to submit review

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const res = require("express/lib/response");
 const req = require("express/lib/request");
+const Review = require("./models/review");
 
 //mongo connection to db
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -105,6 +106,18 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+  })
+);
+
+app.post(
+  "/campgrounds/:id/reviews",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
