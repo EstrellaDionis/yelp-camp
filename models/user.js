0bdee1329e0ccbcdeafcb43f716d89aad08b1956
EstrellaDionis <EstrellaDionis@gmail.com>
2022-05-19 19:05:21 -0400

    created a user model with passport to allow the creation for users

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+//this is going to add on username and password and make sure usernames are unique etc
+//all of this is coming from the passport package
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
