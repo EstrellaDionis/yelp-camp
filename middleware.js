fe17d621226e4292143a928b09feeeac61ee3885
EstrellaDionis <EstrellaDionis@gmail.com>
2022-05-21 18:09:56 -0400

    wip

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ const req = require("express/lib/request");
 
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    //.isAuthenticated is a passport method which checks if they're logged in
     req.flash("error", "you must be signed in");
     return res.redirect("/login");
   }
