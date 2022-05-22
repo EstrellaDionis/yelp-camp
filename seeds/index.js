5af794d559731f39b626a1a035773ff411f0c7f5
EstrellaDionis <EstrellaDionis@gmail.com>
2022-05-22 17:53:59 -0400

    adding ability to see author of a campground by referencing author in campground model, populating author when getting a specific campground and also adding the user (bob) to be the owner of them in seeded campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "6289597b88f53260dd5202ad",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
