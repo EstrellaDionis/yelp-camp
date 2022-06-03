dc41d6a297e5fc67f972c35929e1ca82d9fb07b9
EstrellaDionis <EstrellaDionis@gmail.com>
2022-06-03 11:49:51 -0400

    created ImageSchema and ImageSchema.virtual to create functionality to have thumbnails instead of rending the whole image when editing images

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema; //just to shorten future references
 
+
+const ImageSchema = new Schema({
+  url:String,
+  filename:String
+})
+
+ImageSchema.virtual('thumbnail').get(function(){
+  return this.url.replace('/upload', '/upload/w_300')
+})
+
 const CampgroundSchema = new Schema({
   title: String,
-  images: [{ url: String, filename: String }], //we're taking the filename because the best way to delete a picture is by the filename
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
